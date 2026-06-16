eeba32cecfa2fa141b6b72368954c9c4a08e61be
Misha <m.pavshenko@gmail.com>
2026-06-16 21:14:08 +0300

    ozon stock coverage 28->14

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ cron.schedule(ozonConfig.cronSchedule, async () => {
     // 2. Срок доставки с 14 дней до 10 дней
     // Apr 1st:
     // вернуть горизонт планирования с 14 на 28 дней
-    await fulfillmentReport(28, 28, 10);
+    // Jun 16th:
+    // изменить горизонт планирования с 28 на 14 дней
+    await fulfillmentReport(28, 14, 10);
     console.log(`Ozon fulfillment report generation completed successfully`);
   } catch (error) {
     console.error(`Ozon fulfillment report generation failed:`, error.message || error);
